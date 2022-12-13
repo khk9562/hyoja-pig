12a80420467e5b73488da9ca8c950821d4133ee0
stella <khk9562@naver.com>
2022-12-13 11:37:23 +0900

    renewal site design commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
-$('.imgslide a:gt(0)').hide();
+// 헤더
+let header = document.querySelector("header");
+let headerHeight = header.offsetHeight;
+
+window.onscroll = function () {
+  let windowTop = window.scrollY;
+  	// 스크롤 세로값이 헤더높이보다 크거나 같으면 
+	// 헤더에 클래스 'drop'을 추가한다
+  if (windowTop >= headerHeight) {
+    header.classList.add("drop");
+  } 
+  // 아니면 클래스 'drop'을 제거
+  else {
+    header.classList.remove("drop");
+  }
+};
+
+
+// 이미지슬라이드
+
+$('.imgslide li:gt(0)').hide();
 setInterval(function(){
-    $('.imgslide a:first-child').fadeOut(200)
-                                .next('a').fadeIn(200)
+    $('.imgslide li:first-child').fadeOut(200)
+                                .next('li').fadeIn(200)
                                 .end().appendTo('.imgslide');
-},5000);
+},3000);
